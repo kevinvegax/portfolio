1caff181ed0d6ca557045460eb376818f5957c86
kevinvegadeveloper <kevinvega.x@gmail.com>
2023-05-12 17:20:01 -0600

    Index update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import Banner from './components/Banner';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import About from './components/About';
-import Services from './components/Services';
-import Work from './components/Work';
 import Contact from './components/Contact';
+import Skills from './components/Skills';
 
 const App = () => {
   return (
@@ -15,10 +14,8 @@ const App = () => {
       <Banner />
       <Nav />
       <About />
-      <Services />
-      <Work />
+      <Skills />
       <Contact />
-      <div className='h-[4000px]'></div>
     </div>
   );
 };
